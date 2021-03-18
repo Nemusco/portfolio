55899f174e40b77bc907e44c44fecb17336dba7a
Nemusco <gustavomorillo98@gmail.com>
2021-03-17 21:22:57 -0430

    Resize images

--- sources/dinamismo.js
+++ sources/dinamismo.js
@@ -24,61 +24,19 @@ function headerBackground(){
 
 //Carga de proyectos
 function projects(){
-	/*const ajax = new Ajax();
-
-	ajax.ready = (status,statusText,response) => {
-		const data = JSON.parse(response);
-		const projects = document.getElementById("projects");
-
-		//Genero las cajas con los datos proporcionados por el archivos json
-		data.forEach( (objeto) => {
-			let container = document.createElement("div");
-			let _class = objeto.lenguaje; 
-			container.setAttribute("class",`col mb-3 mb-md-0 d-block ${_class}`);
-
-			let containerFluid = document.createElement("div");
-			containerFluid.setAttribute("class","oxford p-2 border rounded shadow-lg");
-
-			let img = document.createElement("img");
-			img.setAttribute("class","img-fluid");
-			img.setAttribute("src",objeto.imagen);
-
-			let info = document.createElement("div");
-			info.setAttribute("class","mt-2 p-md-2");
-
-			let content = `
-			<h4>${objeto.titulo}</h4>
-			<span class="h5">Frameworks: </span>
-			<strong> ${objeto.frameworks} </strong><br>
-			<span class="h5">Languages:</span>
-			<strong> ${objeto.lenguajes}</strong><br>
-			<span class="h5">Link:</span> 
-			<a href="${objeto.url}" class="text-decoration-none" target="_blank">Click here</a><br>
-			<span class="monospace">${objeto.observacion}</span>
-			`;
-
-			info.innerHTML = content;
-			containerFluid.appendChild(img);
-			containerFluid.appendChild(info);
-			container.appendChild(containerFluid);
-			projects.appendChild(container);
-		});
-	}
-	
-	ajax.request("POST","sources/projects.json","text");*/
 	jQuery.support.cors = true;
 
-	$(document).ready(function() {
+	$(document).ready(() => {
 		$.ajax({
 			type: "GET",
 			url: "sources/projects.json",
 			data: "{}",
 			contentType: "application/json; charset=utf-8",
 			dataType: "json",
-			success: function(data) {
+			success: data => {
 				const projects = document.getElementById("projects");
-				
-				data.forEach( (objeto) => {
+
+				data.forEach( objeto => {
 					let container = document.createElement("div");
 					let _class = objeto.lenguaje; 
 					container.setAttribute("class",`col mb-3 mb-md-0 d-block ${_class}`);
